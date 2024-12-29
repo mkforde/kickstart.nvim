59ad1808b120d41c6653b8b3b06fb51c30f49a58
Michael Forde <michael.kenneth.forde@gmail.com>
2024-12-28 23:48:58 -0500

    More completions and update lazy-lock

--- init.lua
+++ init.lua
@@ -972,5 +972,6 @@ require('lazy').setup({
 })
 
 require('mini.starter').setup()
+require('mini.completion').setup()
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
