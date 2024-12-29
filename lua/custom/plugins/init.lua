ca61c3b340b668fc20e49156f5d2a90882cf7c2e
Michael Forde <michael.kenneth.forde@gmail.com>
2024-12-28 23:13:44 -0500

    Add Markdown Preview and Ignore lazy-lock.json

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,4 +9,13 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
 }
